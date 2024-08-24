2ba82efd2d2cbecebbb9b907b34868484cc591ba
miles <41658201+bizfish@users.noreply.github.com>
2024-08-24 00:36:54 -0500

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -213,7 +213,6 @@ class PiDeskHub:
             async with aiohttp.ClientSession() as session:
                 async with session.get(url) as response:
                     if response.status == 200:
-                        print(f"Response: {await response.text()}")
                         self.remote_connected = True
                     else:
                         print(
